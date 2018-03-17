fd19e4ad422d214fda6353dda7bc102eb383380e
Ben Rodenhäuser <ben@karo.berlin>
2018-03-17 20:50:02 +0100

    add gitignore

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE lists(
   id serial PRIMARY KEY,
-  name text NOT NULL UNIQUE 
+  name text NOT NULL UNIQUE
 );
 
 CREATE TABLE todos(
   id serial PRIMARY KEY,
   task text NOT NULL,
   completed boolean NOT NULL DEFAULT false,
-  list_id NOT NULL REFERENCES lists (id)
+  list_id integer NOT NULL REFERENCES lists (id)
 );
